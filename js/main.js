490d532558f6dc0c7d6514053e70c01df1219be0
Pawel <pawel.markowicz@gmail.com>
2022-09-21 14:27:31 +0200

    dreams 3; components

--- js/main.js
+++ js/main.js
@@ -3,25 +3,32 @@ const navBtn = document.querySelector('.burger-btn');
 const allNavItems = document.querySelectorAll('.nav__item')
 
 const handleNav = () => {
-    nav.classList.toggle('nav--active')
+    // chowa/wyciąga menu po kliknięciu w hamburger
+    nav.classList.toggle('nav--active') 
 
+    // chowa menu po kliknięciu w item/link
     allNavItems.forEach (item => {
         item.addEventListener('click', () => {
             nav.classList.remove('nav--active')
         })
     })
 
+    // na końcu wywołuje funkcje animacji
     handleNavItemsAnimation();
 }
 
+// animacja
 const handleNavItemsAnimation = () => {
     let delayTime = 0;
 
     allNavItems.forEach(item => {
+        // dodaje klase animacji
         item.classList.toggle('nav-items-animation')
+        // następnie każdy item/link dostaje 0.1s opóźnienia 
         item.style.animationDelay = '.' + delayTime + 's';
         delayTime ++;
     })
 }
 
+// nasłuchiwanie na burger ikonę
 navBtn.addEventListener('click', handleNav)
