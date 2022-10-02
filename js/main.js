a51188d0a55b3968e8c9b21f2b700d94265c91f8
Pawel Markowicz <pawel.markowicz88@gmail.com>
2022-10-02 19:47:04 +0200

    JS: changing year in footer and changing color of burger menu: if background white, bars are black

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const nav = document.querySelector('.nav');
 const navBtn = document.querySelector('.burger-btn');
-const allNavItems = document.querySelectorAll('.nav__item')
+const allNavItems = document.querySelectorAll('.nav__item');
+const navBtnBars = document.querySelector('.burger-btn__bars');
+const allSections = document.querySelectorAll('.section');
+const footerYear = document.querySelector('.footer__year');
 
 const handleNav = () => {
     // chowa/wyciąga menu po kliknięciu w hamburger
@@ -30,5 +33,41 @@ const handleNavItemsAnimation = () => {
     })
 }
 
+// zmiana roku w footerze
+
+const handleCurrentYear = () => {
+    // const year to funkcja; new Date to obiekt nowej daty; getFullYear to metoda;
+
+    const year = (new Date).getFullYear();
+    footerYear.innerText = year;
+}
+
+const handleObserver = () => {
+    const currentSection = window.scrollY;
+
+    // dzięki temu na każdej sekcji jest wykonany poniższy kod 
+    allSections.forEach(section =>{
+
+        // jeśli sekcja posiada klasę white-section, sprawdza to.
+        // && oraz czy górna krawędź sekcji jest mniejsza lub równa currentSection, czy wesła na sekcje 
+        if(section.classList.contains('white-section') && section.offsetTop <= currentSection + 60){
+            navBtnBars.classList.add('black-bars-color')
+
+              // jeśli sekcja na której jesteśmy nie posiada white-section ale dotknęła krawędzi przeglądarki to ma wykonać kod
+        } else if (!section.classList.contains('white-section') && section.offsetTop <= currentSection + 60){
+            navBtnBars.classList.remove('black-bars-color');
+        }
+
+    })
+}
+
+
+// wywołanie funkci zmiany koloru burgera
+
+window.addEventListener('scroll', handleObserver);
+
+// wywołanie funkcji zmiany daty
+handleCurrentYear();
+
 // nasłuchiwanie na burger ikonę
-navBtn.addEventListener('click', handleNav)
+navBtn.addEventListener('click', handleNav);
